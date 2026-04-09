e6945622fbb65a0e59ed8b816f0b61c04746118b
thanhbtse162065 <thanhbuituan1915@gmail.com>
2026-04-09 13:58:45 +0700

    end ò section 5 - sort, search, filter, pagination

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,22 +1,24 @@
 using Core.Entities;
 using Core.Interfaces;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using API.RequestHelpers;
+using Core.Specifications;
+
+
 namespace API.Controllers;
 
-using Core.Specifications;
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
 {
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(
+        [FromQuery] ProductSpecParams specParams)
     {
-        var spec = new ProductSpecification(brand, type, sort);
-        var products = await repo.ListAsync(spec);
-        return Ok(products);
+        var spec = new ProductSpecification(specParams);
+
+        return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
     }
 
     [HttpGet("{id:int}")] // api/products/3
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -49,7 +49,7 @@ public class ProductRepository(StoreContext context) : IProductRepository
                 _ => query.OrderBy(x => x.Name)
             };
         }
-        return await query.ToListAsync();
+        return await query.Skip(5).Take(5).ToListAsync();
     }
 
     public async Task<IReadOnlyList<string>> GetTypesAsync()
