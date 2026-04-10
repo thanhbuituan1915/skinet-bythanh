2535f43f16f1825162c5e6088c3ec8b75782b291
thanhbtse162065 <thanhbuituan1915@gmail.com>
2026-04-10 21:48:53 +0700

    end of section 6 - error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,15 +14,15 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
-
-
-
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
